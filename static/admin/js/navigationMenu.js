2e8a78779100dafab9a624bed7b7d3712f04066c
christian-skeptics <christianskeptics@gmail.com>
2025-05-11 18:29:18 -0600

    Add content

--- static/admin/js/navigationMenu.js
+++ static/admin/js/navigationMenu.js
@@ -11,14 +11,10 @@ function createNavigationMenu()
     .append($('<div>').attr({"class":"row"}).append($('<a>').attr({"href":"ContradictionOrParadox.html"}).text("Contradiction Or Paradox?").css({"fontSize":20,"font-weight":"bold"})))
     .append($('<div>').attr({"class":"row"}).append($('<a>').attr({"href":"QuestioningFaith.html"}).text("Questioning Faith").css({"fontSize":20,"font-weight":"bold"})))
     .append($('<div>').attr({"class":"row"}).append($('<a>').attr({"href":"Definitions.html"}).text("Definitions").css({"fontSize":20,"font-weight":"bold"})))
-    .append($('<div>').attr({"class":"row"}).append($('<a>').attr({"href":"CriticalThinking.html"}).text("Critical Thinking").css({"fontSize":20,"font-weight":"bold"})))
-	.append($('<div>').attr({"class":"row"}).append($('<div>').attr({"class":"col-lg-2"})).append($('<div>').attr({"class":"col-lg-4"}).text("Placeholder").css({"fontSize":20,"font-weight":"bold"})))
-	.append($('<div>').attr({"class":"row"}).append($('<div>').attr({"class":"col-lg-2"})).append($('<div>').attr({"class":"col-lg-4"}).text("Placeholder").css({"fontSize":20,"font-weight":"bold"})))
-		
-    .append($('<div>').attr({"class":"row"}).append($('<a>').attr({"href":"MindsetApproach.html"}).text("Mindset And Approach").css({"fontSize":20,"font-weight":"bold"})))
-    .append($('<div>').attr({"class":"row"}).append($('<a>').attr({"href":"Faith.html"}).text("Faith").css({"fontSize":20,"font-weight":"bold"})))
-    .append($('<div>').attr({"class":"row"}).append($('<div>').attr({"class":"col-lg-2"})).append($('<div>').attr({"class":"col-lg-4"}).text("Placeholder").css({"fontSize":20,"font-weight":"bold"})))
-	.append($('<div>').attr({"class":"row"}).append($('<div>').attr({"class":"col-lg-2"})).append($('<div>').attr({"class":"col-lg-4"}).text("Placeholder").css({"fontSize":20,"font-weight":"bold"})))
+    .append($('<div>').attr({"class":"row", "id":"CriticalThinking"}).append($('<a>').attr({"href":"CriticalThinking.html"}).text("Critical Thinking").css({"fontSize":20,"font-weight":"bold"})))
+    //.append($('<div>').attr({"class":"row"}).append($('<a>').attr({"href":"MindsetApproach.html"}).text("Mindset And Approach").css({"fontSize":20,"font-weight":"bold"})))
+    .append($('<div>').attr({"class":"row", "id":"Faith"}).append($('<a>').attr({"href":"Faith.html"}).text("Faith").css({"fontSize":20,"font-weight":"bold"})))
+    .append($('<div>').attr({"class":"row", "id":"About"}).append($('<a>').attr({"href":"About.html"}).text("About").css({"fontSize":20,"font-weight":"bold"})))
 
   );
 }
